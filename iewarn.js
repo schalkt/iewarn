61d9208e2895328a7c00531f23dd630afecdaa0a
schalkt <email@schalk.hu>
2013-06-30 18:03:16 +0200

    new style

--- iewarn.js
+++ iewarn.js
@@ -1,17 +1,13 @@
 /*
  * iewarn.js
- * v1.0 - 2013.05.17
+ * v1.1 - 2013.06.30
  *
- * The iewarn is a javascript that displays a warning message on the webpage
- * bottom
- * for informing the visitor to upgrade the Internet Explorer to a newer version
- * or
- * install another browser.
+ * The iewarn is a javascript code that displays a warning message
+ * on the webpage bottom for informing the visitor to upgrade
+ * the Internet Explorer to a newer version or install another browser.
  *
- *
- * <!--[if lt IE 8]>
- *   <script type="text/javascript"
- * src="/[path_to_js_files]/iewarning/iewarning.js" ></script>
+ * <!--[if lte IE 7]>
+ *   <script type="text/javascript" src="/js/iewarn/iewarn.js"></script>
  * <![endif]-->
  *
  *
@@ -20,28 +16,33 @@
  *
  */
 
-var iewarn = {
+(function(fn) {
 
-    // url for the browser icons - CHANGE THIS!!!
-    path : '/swems/include/iewarn/',
+    var expire, message, button, browsers, close, cookie;
 
     // the cookie will expire after this (day), set to null for session expire
-    expire : 1,
+    expire = 1;
+
+    // the title
+    title = {
+        'hu-hu' : "Tud róla, hogy az Ön Internet Explorer böngészője elavult?",
+        'en-us' : "Did you know that your Internet Explorer is out of date?"
+    };
 
     // the message
-    message : {
-        'hu-hu' : "<h1>Tud róla, hogy az Ön Internet Explorer böngészője elavult?</h1>Weboldalunk az Ön képernyőjén hibásan jelenhet meg! A jobb böngészési élményért és a biztonságos internethasználathoz javasoljuk, hogy frissítse Internet Explorer-ét a legújabb verzióra, vagy próbáljon ki más korszerű böngészőprogramot. Az alábbi lista tartalmazza a legnépszerűbb, ingyenesen elérhető alternatívákat. Letöltéshez kattintson az ikonok egyikére.",
-        'en-us' : "<h1>Did you know that your Internet Explorer is out of date?</h1>Our website may be displayed incorrectly on your screen! To get safe and best possible browsing experience we recommend that you upgrade to a newer version or other web browser. A list of the most popular web browsers can be found below. Just click on the icons to get to the download page."
-    },
+    message = {
+        'hu-hu' : "Weboldalunk az Ön képernyőjén hibásan jelenhet meg! A jobb böngészési élményért és a biztonságos internethasználathoz javasoljuk, hogy frissítse Internet Explorer-ét a legújabb verzióra, vagy próbáljon ki más korszerű böngészőprogramot. Az alábbi lista tartalmazza a legnépszerűbb, ingyenesen elérhető alternatívákat. Letöltéshez kattintson az ikonok egyikére.",
+        'en-us' : "Our website may be displayed incorrectly on your screen! To get safe and best possible browsing experience we recommend that you upgrade to a newer version or other web browser. A list of the most popular web browsers can be found below. Just click on the icons to get to the download page."
+    };
 
     // the close button
-    button : {
-        'hu-hu' : ' Amennyiben most nem szeretné, kattintson erre: <span id="iewarning-close" onclick="javascript: iewarn.close();">Későbbi emlékeztetést kérek!</span>',
-        'en-us' : ' <span id="iewarning-close" onclick="javascript: iewarn.close();">Remind me later.</span>'
-    },
+    button = {
+        'hu-hu' : ' Figyelmeztetés bezárása és emkékeztetés később.',
+        'en-us' : ' Remind me later.'
+    };
 
-    // browser name, link and icon
-    browsers : {
+    // browser name, url and icon
+    browsers = {
         ff : {
             name : "Firefox",
             url : "http://www.mozilla.com/firefox/",
@@ -74,74 +75,45 @@ var iewarn = {
 
         }
 
-    },
+    };
 
-    // show the warning box
-    show : function() {
+    // close the warning box
+    window[fn] = {
 
-        if (this.cookie('iewarning'))
-            return;
+        close : function() {
 
-        var body, iebox, browser, browsers, language, img, link;
+            // set the cookie
+            cookie('iewarn', true);
 
-        // get browser language
-        language = window.navigator.userLanguage || window.navigator.language;
-        language = language.toLowerCase();
-
-        if (language == 'hu')
-            language = 'hu-hu';
-
-        if (!this.message[language])
-            language = 'en-us';
-
-        body = document.getElementsByTagName("body")[0];
-        iebox = document.createElement('div');
-        browsers = '';
-
-        for (key in this.browsers) {
-
-            browser = this.browsers[key];
-            img = '<img width="18" src="' + browser.icon + '" alt="" />';
-            link = '<a href="' + browser.url + '" target="_blank">' + img + '<br />' + browser.name + '</a>';
-            browsers = browsers + '<td>' + link + '</td>';
+            // remove warning box
+            var obj = document.getElementById('iewarn');
+            if (obj) {
+                obj.parentNode.removeChild(obj);
+            }
 
         }
 
-        iebox.setAttribute("id", "iewarning");
-        iebox.innerHTML = this.message[language] + this.button[language] + '<table width="10%"><tr>' + browsers + '</tr></table>';
-        body.appendChild(iebox);
-
-    },
-
-    // close the warning box
-    close : function() {
-
-        this.cookie('iewarning', true);
-        
-        var obj = document.getElementById('iewarning');
-
-        if (obj)
-            obj.parentNode.removeChild(obj);
-
-    },
+    };
 
     // set and get cookie function
-    cookie : function(name, value, days) {
+    cookie = function(name, value, days) {
 
-        if (days == undefined)
-            days = this.expire;
+        if (days == undefined) {
+            days = expire;
+        }
 
         // if value not undefined set the cookie else read
         if (value !== undefined) {
 
             var expire = new Date();
             expire.setDate(expire.getDate() + days);
-            var value = escape(value) + ((days == null) ? "" : "; expires=" + expire.toGMTString());
+            value = escape(value) + ((days == null) ? "" : "; expires=" + expire.toGMTString());
             document.cookie = name + "=" + value;
 
         } else {
 
             var i, x, y, cookies = document.cookie.split(";");
+
             for ( i = 0; i < cookies.length; i++) {
                 x = cookies[i].substr(0, cookies[i].indexOf("="));
                 y = cookies[i].substr(cookies[i].indexOf("=") + 1);
@@ -155,6 +127,50 @@ var iewarn = {
 
     }
 
-}
+    // if cookie true return
+    if (cookie('iewarn')) {
+        return;
+    }
 
-iewarn.show();
+    var body, iebox, browser, language, img, link, list, html;
+
+    // get browser language
+    language = window.navigator.userLanguage || window.navigator.language;
+    language = language.toLowerCase();
+
+    if (language == 'hu') {
+        language = 'hu-hu';
+    }
+
+    if (!message[language]) {
+        language = 'en-us';
+    }
+
+    // get body and create warning box
+    body = document.getElementsByTagName("body")[0];
+    iebox = document.createElement('div');
+    list = '';
+
+    // generate browsers informations
+    for (key in browsers) {
+
+        browser = browsers[key];
+        img = '<img src="' + browser.icon + '" alt="" />';
+        link = '<a href="' + browser.url + '" target="_blank">' + img + browser.name + '</a>';
+        list = list + link;
+
+    }
+
+    // set warning box id and html
+    iebox.setAttribute("id", "iewarn");
+    button = '<span id="iewarn-close" onclick="javascript: iewarn.close();">' + button[language] + '</span>';
+    html = '<h1>' + title[language] + '</h1>';
+    html += '<div class="text">' + message[language] + '<div class="list">' + list + '</div>' + button + '</div>';
+
+    iebox.innerHTML = html;
+
+    // append to body
+    body.appendChild(iebox);
+
+})('iewarn');
+
